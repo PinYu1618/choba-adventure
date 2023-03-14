3b0968e641a0b2b394fbbca4d7a40ecc5b5d2049
PinYu1618 <pinyuchen658503@gmail.com>
2023-03-15 03:17:02 +0800

    remove unused deps, and migrating to bevy_ecs_ldtk

---
+++ examples/hello.rs
@@ -0,0 +1,19 @@
+use bevy::prelude::*;
+use bevy_ecs_ldtk::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(LdtkPlugin)
+        .add_startup_system(setup)
+        .insert_resource(LevelSelection::Index(1))
+        .run();
+}
+
+fn setup(mut cmds: Commands, ass: Res<AssetServer>) {
+    cmds.spawn(Camera2dBundle::default());
+    cmds.spawn(LdtkWorldBundle {
+        ldtk_handle: ass.load("ldtks/Test.ldtk"),
+        ..default()
+    });
+}
